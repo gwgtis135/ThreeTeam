e409610cee59eda73329aa46b9c20506d956a3c9
admin <admin@YD01-07>
2021-11-03 12:28:03 +0900

    first

---
+++ proj/src/main/java/co/hanki/prj/command/Test.java
@@ -0,0 +1,16 @@
+package co.hanki.prj.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import co.hanki.prj.comm.Command;
+
+public class Test implements Command {
+
+	@Override
+	public String run(HttpServletRequest request, HttpServletResponse response) {
+		
+		return "home.do";
+	}
+
+}
